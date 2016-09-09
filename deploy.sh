78ec0624e62814cb8269939b07cbe3b8d0bc6e86
justindav1s <justin.davis@ba.com>
2016-09-09 21:14:19 +0100

    deploy script updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo >>>>> This is the deployer script.
-echo >>>>> it run in the s2i builder image and does what it akes to build the application :
-echo >>>>> * building it ...
-echo >>>>> * sucking prebuilt components out of nexus
+echo "---- This is the deployer script."
+echo "---- it run in the s2i builder image and does what it akes to build the application :"
+echo "---- * building it ..."
+echo "---- * sucking prebuilt components out of nexus"
+echo "---- * ansible ?"
