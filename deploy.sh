760f9ca08cfb463d32e88bdcad951e9a0461d7b6
Justin Davis <justinndavis@gmail.com>
2016-09-09 20:18:20 +0100

    created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo >>>>> This is the deployer script.
+echo >>>>> it run in the s2i builder image and does what it akes to build the application :
+echo >>>>> * building it ...
+echo >>>>> * sucking prebuilt components out of nexus
