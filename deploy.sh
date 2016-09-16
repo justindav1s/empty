61fb22bd5e4b3715b70e99c2ea9d47445e3c73d5
justindav1s <justin.davis@ba.com>
2016-09-16 17:17:23 +0100

    deploy stuff

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ echo "---- it run in the s2i builder image and does what it akes to build the ap
 echo "---- * building it ..."
 echo "---- * sucking prebuilt components out of nexus"
 echo "---- * ansible ?"
+
+
+wget http://nexus-ci.ba.datr.eu/content/repositories/snapshots/com/ba/captwo/eda/demo/selling-services/1.0-SNAPSHOT/selling-services-1.0-20160916.161135-1.war
