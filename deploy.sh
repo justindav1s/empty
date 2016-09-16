3cb1a0df890b00facec8f66d0f598ec67a2916f3
justindav1s <justin.davis@ba.com>
2016-09-16 21:57:29 +0100

    deploy stuff

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,14 @@ echo "---- * building it ..."
 echo "---- * sucking prebuilt components out of nexus"
 echo "---- * ansible ?"
 
+wget http://central.maven.org/maven2/ch/qos/logback/logback-classic/1.1.7/logback-classic-1.1.7.jar
+mv logback-classic-1.1.7.jar /ib/appl/tomcat7/lib
+
+wget http://central.maven.org/maven2/ch/qos/logback/logback-core/1.1.7/logback-core-1.1.7.jar
+mv logback-core-1.1.7.jar /ib/appl/tomcat7/lib
+
+wget http://central.maven.org/maven2/org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar
+mv slf4j-api-1.7.21.jar /ib/appl/tomcat7/lib
 
 wget http://172.30.39.124:8081/content/repositories/snapshots/com/ba/captwo/eda/demo/selling-services/1.0-SNAPSHOT/selling-services-1.0-20160916.161135-1.war
-
 mv selling-services-1.0-20160916.161135-1.war  /ib/appl/tomcat7/webapps/selling.war
