c39a409e0494b10122924d7c5b15fcd5a1673a6b
justindav1s <justin.davis@ba.com>
2016-09-16 21:58:57 +0100

    deploy stuff

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,8 @@ mv logback-core-1.1.7.jar /ib/appl/tomcat7/lib
 wget http://central.maven.org/maven2/org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar
 mv slf4j-api-1.7.21.jar /ib/appl/tomcat7/lib
 
+wget http://central.maven.org/maven2/org/apache/camel/camel-core/2.17.3/camel-core-2.17.3.jar
+mv camel-core-2.17.3.jar /ib/appl/tomcat7/lib
+
 wget http://172.30.39.124:8081/content/repositories/snapshots/com/ba/captwo/eda/demo/selling-services/1.0-SNAPSHOT/selling-services-1.0-20160916.161135-1.war
 mv selling-services-1.0-20160916.161135-1.war  /ib/appl/tomcat7/webapps/selling.war
