d017baf19d238234deaa09a393407f2bf276608c
justindav1s <justin.davis@ba.com>
2016-09-17 18:45:25 +0100

    sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+CREATE SEQUENCE BA_SEQ INCREMENT BY 1 MINVALUE 1000 MAXVALUE 999999 START WITH 1000 CYCLE;
+CREATE TABLE persons(PersonID int,lastName varchar(255),firstName varchar(255),address varchar(255),city varchar(255), PRIMARY KEY (PersonID));
+CREATE TABLE bookings (personID int,bookingId int,flightnum varchar(255),tickets int,cabin  varchar(255), PRIMARY KEY (bookingId));
+CREATE TABLE flights(flightnum varchar(255),origin varchar(255),destination varchar(255),PRIMARY KEY (flightnum));
+CREATE TABLE loyalty(PersonID int, tier varchar(255), points int);
