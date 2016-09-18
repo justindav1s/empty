288c2c47025a9cf23bf99547e3ecc872f7632f47
justindav1s <justin.davis@ba.com>
2016-09-18 21:51:02 +0100

    redis

---
+++ selling-services/src/main/java/com/ba/captwo/eda/demo/redis/RedisDAO.java
@@ -0,0 +1,26 @@
+package com.ba.captwo.eda.demo.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by justin on 18/09/2016.
+ */
+@Component("RedisDAO")
+public class RedisDAO {
+
+    private final Logger log = LoggerFactory.getLogger(RedisDAO.class);
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    public void addProperty(String key, String value)   {
+        redisTemplate.boundListOps(key).leftPush(value);
+    }
+}
