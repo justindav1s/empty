6eeadc546b661491788e8dfb4b74a7cb1a1fb636
justindav1s <justin.davis@ba.com>
2016-09-18 13:40:12 +0100

    loging fixes

--- data/data.sql
+++ data/data.sql
@@ -1,13 +1,3 @@
-CREATE USER u760245 SUPERUSER LOGIN PASSWORD 'u760245';
-CREATE DATABASE selling with OWNER u760245;
-\connect DBNAME
-CREATE SEQUENCE BA_SEQ INCREMENT BY 1 MINVALUE 1000 MAXVALUE 999999 START WITH 1000 CYCLE;
-CREATE TABLE persons(PersonID int,lastName varchar(255),firstName varchar(255),address varchar(255),city varchar(255), PRIMARY KEY (PersonID));
-CREATE TABLE bookings (personID int,bookingId int,flightnum varchar(255),tickets int,cabin  varchar(255), PRIMARY KEY (bookingId));
-CREATE TABLE flights(flightnum varchar(255),origin varchar(255),destination varchar(255),PRIMARY KEY (flightnum));
-CREATE TABLE loyalty(PersonID int, tier varchar(255), points int);
-
-
 CREATE USER justin SUPERUSER LOGIN PASSWORD 'justin';
 CREATE DATABASE selling with OWNER justin;
 GRANT ALL PRIVILEGES ON TABLE bookings TO justin
@@ -17,3 +7,9 @@ CREATE TABLE persons(PersonID int,lastName varchar(255),firstName varchar(255),a
 CREATE TABLE bookings (personID int,bookingId int,flightnum varchar(255),tickets int,cabin  varchar(255), PRIMARY KEY (bookingId));
 CREATE TABLE flights(flightnum varchar(255),origin varchar(255),destination varchar(255),PRIMARY KEY (flightnum));
 CREATE TABLE loyalty(PersonID int, tier varchar(255), points int);
+GRANT ALL PRIVILEGES ON SEQUENCE BA_SEQ TO justin;
+GRANT ALL PRIVILEGES ON TABLE persons TO justin;
+GRANT ALL PRIVILEGES ON TABLE bookings TO justin;
+GRANT ALL PRIVILEGES ON TABLE flights TO justin;
+GRANT ALL PRIVILEGES ON TABLE loyalty TO justin;
+INSERT INTO FLIGHTS (flightnum, origin, destination) values ('BA009', 'LHR', 'BKK');
