49b88d983dabeb68e20507dec85fd49f7e3b6e84
Justin Davis <justin.davis@ba.com>
2016-09-19 19:19:24 +0200

    redis fixes

--- selling-services/src/main/java/com/ba/captwo/eda/demo/redis/RedisDAO.java
+++ selling-services/src/main/java/com/ba/captwo/eda/demo/redis/RedisDAO.java
@@ -3,6 +3,7 @@ package com.ba.captwo.eda.demo.redis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,10 +18,8 @@ public class RedisDAO {
     private final Logger log = LoggerFactory.getLogger(RedisDAO.class);
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    public RedisConfig config;
 
 
-    public void addProperty(String key, String value)   {
-        redisTemplate.boundListOps(key).leftPush(value);
-    }
+
 }
