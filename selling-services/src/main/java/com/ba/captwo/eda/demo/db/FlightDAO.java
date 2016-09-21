ed3d737fe43c2fff6033934af1853e8667994457
Justin Davis <justin.davis@ba.com>
2016-09-21 13:18:03 +0200

    redis fixes

--- selling-services/src/main/java/com/ba/captwo/eda/demo/db/FlightDAO.java
+++ selling-services/src/main/java/com/ba/captwo/eda/demo/db/FlightDAO.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
  * Created by u760245 on 04/07/2014.
  */
 @Component("FlightDAO")
-public class FlightDAO implements FlightService{
+public class FlightDAO {
 
     private final Logger log = LoggerFactory.getLogger(FlightDAO.class);
 
