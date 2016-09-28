5294c42dfb5808233f4aefe03c176be5d2b6aef6
Justin Davis <justin.davis@ba.com>
2016-09-28 14:58:24 +0200

    fixed cache add entry method

--- selling-services/src/main/java/com/ba/captwo/eda/demo/resources/CacheResourceImpl.java
+++ selling-services/src/main/java/com/ba/captwo/eda/demo/resources/CacheResourceImpl.java
@@ -3,6 +3,8 @@ package com.ba.captwo.eda.demo.resources;
 import com.ba.captwo.eda.demo.coreservices.CacheService;
 import com.ba.captwo.eda.demo.coreservices.PersonService;
 import com.ba.captwo.eda.demo.model.Error;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.MatrixParam;
@@ -17,11 +19,22 @@ public class CacheResourceImpl implements CacheResource{
     @Autowired
     CacheService cacheService;
 
+    private final Logger log = LoggerFactory.getLogger(CacheResource.class);
+
     @Override
     public Response addEntry(String val) {
+
+        ResourceRquest rr = new ResourceRquest();
+        rr.putNVP("type", "REQUEST");
+        rr.putNVP("op", "addEntry");
+        rr.putNVP("method", "GET");
+        rr.putNVP("val", val);
+        log.debug(rr.toJson());
+
         Response response = null;
+        ArrayList<String> list = null;
         try {
-            ArrayList<String> list = cacheService.addEntry(val);
+            list = cacheService.addEntry(val);
             response = Response.status(Response.Status.OK).entity(list).build();
         }
         catch (Exception e) {
@@ -30,14 +43,21 @@ public class CacheResourceImpl implements CacheResource{
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(err).build();
         }
 
+        rr = new ResourceRquest();
+        rr.putNVP("type", "RESPONSE");
+        rr.putNVP("op", "addEntry");
+        rr.putNVP("method", "GET");
+        rr.putNVP("response", list.toString());
+        log.debug(rr.toJson());
         return response;
     }
 
     @Override
     public Response listEntries() {
         Response response = null;
+        ArrayList<String> list = null;
         try {
-            ArrayList<String> list = cacheService.listEntries();
+            list = cacheService.listEntries();
             response = Response.status(Response.Status.OK).entity(list).build();
         }
         catch (Exception e) {
@@ -45,6 +65,13 @@ public class CacheResourceImpl implements CacheResource{
             err.setMessage(e.getMessage());
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(err).build();
         }
+
+        ResourceRquest rr = new ResourceRquest();
+        rr.putNVP("type", "RESPONSE");
+        rr.putNVP("op", "addEntry");
+        rr.putNVP("method", "GET");
+        rr.putNVP("response", list.toString());
+        log.debug(rr.toJson());
 
         return response;
     }
@@ -52,8 +79,9 @@ public class CacheResourceImpl implements CacheResource{
     @Override
     public Response clearEntries() {
         Response response = null;
+        ArrayList<String> list = null;
         try {
-            ArrayList<String> list = cacheService.clearEntries();
+            list = cacheService.clearEntries();
             response = Response.status(Response.Status.OK).entity(list).build();
         }
         catch (Exception e) {
@@ -61,6 +89,13 @@ public class CacheResourceImpl implements CacheResource{
             err.setMessage(e.getMessage());
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(err).build();
         }
+
+        ResourceRquest rr = new ResourceRquest();
+        rr.putNVP("type", "RESPONSE");
+        rr.putNVP("op", "addEntry");
+        rr.putNVP("method", "GET");
+        rr.putNVP("response", list.toString());
+        log.debug(rr.toJson());
 
         return response;
     }
