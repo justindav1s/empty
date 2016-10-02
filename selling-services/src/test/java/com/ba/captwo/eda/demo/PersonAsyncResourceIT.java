92e52db9ec53008a66d54f1e419d02976b575c70
justindav1s <justin.davis@ba.com>
2016-10-02 21:39:12 +0100

    openshift build stuff

--- selling-services/src/test/java/com/ba/captwo/eda/demo/PersonAsyncResourceIT.java
+++ selling-services/src/test/java/com/ba/captwo/eda/demo/PersonAsyncResourceIT.java
@@ -7,8 +7,11 @@ import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
@@ -18,6 +21,8 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/test_beans.xml"})
 public class PersonAsyncResourceIT {
     private static String endpointUrl;
     private final static Logger log = LoggerFactory.getLogger(PersonAsyncResourceIT.class);
