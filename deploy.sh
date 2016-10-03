6f2d117ff22d2017d7b959d87610e038c0e3a6c1
Justin Davis <justin.davis@ba.com>
2016-10-03 17:37:57 +0200

    updated db connecton parms

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cp /tmp/src/scripts/setenv.sh /ib/appl/tomcat7/bin
 
 cp /tmp/src/scripts/logback.xml /ib/appl/tomcat7/conf
 
-wget "http://172.30.39.124:8081/service/local/artifact/maven/redirect?r=snapshots&g=com.ba.captwo.eda.demo&a=selling-services&v=LATEST&p=war" -O selling.war
+wget "http://172.30.158.160:8081/service/local/artifact/maven/redirect?r=snapshots&g=com.ba.captwo.eda.demo&a=selling-services&v=LATEST&p=war" -O selling.war
 mv selling.war  /ib/appl/tomcat7/webapps/selling.war
